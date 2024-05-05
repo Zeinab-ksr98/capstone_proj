ab472defc5933ae51a3e6f65a46200d59fe58445
Zeinab-ksr98 <zeinabksr98@gmail.com>
2024-05-05 14:12:39 +0300

    whatsappp

---
+++ Sa7tak3ena/src/main/java/com/dgpad/thyme/repository/ImageRepository.java
@@ -0,0 +1,10 @@
+package com.dgpad.thyme.repository;
+
+import com.dgpad.thyme.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+}
